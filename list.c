49ab0fe1c775fa3e05094291ec63a1f7d89c6a77
Dario Lombardo <lomato@gmail.com>
2014-01-23 15:16:35 +0100

    Fragments fully supported

--- list.c
+++ list.c
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 #include <log.h>
 
-fragnode_t* fragnode_create(uint16_t ipid, size_t size)
+static void fragnode_print(fragnode_t* fragnode)
+{
+    printf("NODE: %p, %u %u %u %lu\n", fragnode, fragnode->ipid, fragnode->src.s_addr,
+        fragnode->dst.s_addr, fragnode->size);
+}
+
+fragnode_t* fragnode_create(uint16_t ipid, struct in_addr src, struct in_addr dst, size_t size)
 {
 	fragnode_t* elem;
 
@@ -13,58 +19,68 @@ fragnode_t* fragnode_create(uint16_t ipid, size_t size)
 		LOG_ERROR("Can't create fragnode");
 		return NULL;
 	}
-	
+
 	elem->ipid = ipid;
+	elem->src = src;
+	elem->dst = dst;
 	elem->size = size;
-	
+	elem->next = NULL;
+
 	return elem;
 }
 
-void fragnode_delete(fragnode_t** head, fragnode_t* fragnode)
+static int fragnode_equal(fragnode_t* e1, fragnode_t* e2)
+{
+    if (e1->ipid == e2->ipid && e1->src.s_addr == e2->src.s_addr &&
+            e1->dst.s_addr == e2->dst.s_addr && e1->size == e2->size)
+        return 0;
+    return 1;
+}
+
+void fragnode_unlink(fragnode_t** head, fragnode_t* fragnode)
 {
     fragnode_t* cur;
-    fragnode_t* d;
+
     if (*head == fragnode) {
-        cur = *head;
-        *head = cur->next;
-        free(cur);
+        *head = (*head)->next;
         return;
     }
-    
+
     cur = *head;
     while(cur->next != NULL) {
-        if (cur->next->ipid == fragnode->ipid && cur->next->size == fragnode->size) {
-            d = cur->next;
+        if (fragnode_equal(cur->next, fragnode)) {
             cur->next = cur->next->next;
-            free(d);
             return;
         }
     }
-    
+
     // not found...
-    LOG_ERROR("Element not found");       
+    LOG_ERROR("Element not found");
 }
 
-fragnode_t* fragnode_update(fragnode_t** head, uint16_t ipid, size_t size)
+fragnode_t* fragnode_update(fragnode_t* head, uint16_t ipid, struct in_addr src,
+    struct in_addr dst, size_t size)
 {
     fragnode_t* cur;
-    
-    if (*head == NULL) {
-        *head = fragnode_create(ipid, size);
-        return *head;
-    }
-    
-    cur = *head;
+
+    cur = head;
     while(cur != NULL) {
-        if (cur->ipid == ipid) {
+        if (cur->ipid == ipid && cur->src.s_addr == src.s_addr && cur->dst.s_addr == dst.s_addr) {
             // Found. Update the size and return
             cur->size += size;
             return cur;
         }
     }
-    
-    // Not found, add a new node
-    cur = fragnode_create(ipid, size);
+
+    LOG_ERROR("%s: Cant' find fragnode\n", __func__);
+    return NULL;
+}
+
+fragnode_t* fragnode_add(fragnode_t** head, uint16_t ipid, struct in_addr src,
+    struct in_addr dst, size_t size)
+{
+    fragnode_t* cur;
+    cur = fragnode_create(ipid, src, dst, size);
     cur->next = *head;
     *head = cur;
     return *head;
--- list.h
+++ list.h
@@ -5,9 +5,12 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <netinet/ip.h>
 
 typedef struct fragnode {
 	uint16_t ipid;
+	struct in_addr src;
+	struct in_addr dst;
 	size_t size;
 	struct fragnode* next;
 } fragnode_t;
@@ -15,6 +18,11 @@ typedef struct fragnode {
 //! \brief Update a fragnode_t list
 //! The updated element is returned.
 //! If the datagram is complete, the value of complete is set to true and the element is deleted from the list
-fragnode_t* fragnode_update(fragnode_t** head, uint16_t ipid, size_t size);
+fragnode_t* fragnode_update(fragnode_t* head, uint16_t ipid, struct in_addr src, struct in_addr dst, size_t size);
+
+fragnode_t* fragnode_add(fragnode_t** head, uint16_t ipid, struct in_addr src,
+    struct in_addr dst, size_t size);
+
+void fragnode_unlink(fragnode_t** head, fragnode_t* fragnode);
 
 #endif
